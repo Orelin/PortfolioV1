f71ab7c9d47afe88d447842ca6150f0001e0b82d
Dahian Sosa <shaco-dark@outlook.com>
2022-10-20 21:37:07 -0300

    Working/learning

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,10 @@ const options = {
     height: window.innerHeight, 
     width: window.innerWidth,
     cellSize: 40,
-    variance: 0.2,
+    variance: 2,
     seed: null,
-    xColors: ["#2C3333", "#395B64", "#A5C9CA", "#E7F6F2"],
+    //xColors: ["#2C3333", "#395B64", "#A5C9CA", "#E7F6F2"], //Neutro 3/4
+    xColors: ["#AAC4FF", "#FFE3E1", "#FFD1D1", "#FF9494"],
     //xColors: "random",
     yColors: "match",
     fill: true,
@@ -16,6 +17,6 @@ const options = {
     strokeWidth: 0,
     points: null
 }
-
+console.log(options.xColors)
 const pattern = trianglify(options)
 document.body.appendChild(pattern.toCanvas())
